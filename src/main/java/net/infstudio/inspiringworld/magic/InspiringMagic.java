ece231af6c4426fd3b11c6008dcb2709405f0eca
Yanbing Zhao <zzzz@mail.ustc.edu.cn>
2016-07-16 22:54:30 +0800

    Initialize mods

---
+++ src/main/java/net/infstudio/inspiringworld/magic/InspiringMagic.java
@@ -0,0 +1,28 @@
+package net.infstudio.inspiringworld.magic;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = InspiringMagic.MODID, version = InspiringMagic.VERSION, acceptedMinecraftVersions = "1.10.2")
+public class InspiringMagic {
+    public static final String MODID = "inspiringmagic";
+    public static final String VERSION = "@version@";
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+	// TODO
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+	// TODO
+    }
+
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+	// TODO
+    }
+}
---
+++ src/main/java/net/infstudio/inspiringworld/tech/InspiringTech.java
@@ -0,0 +1,28 @@
+package net.infstudio.inspiringworld.tech;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = InspiringTech.MODID, version = InspiringTech.VERSION, acceptedMinecraftVersions = "1.10.2")
+public class InspiringTech {
+    public static final String MODID = "inspiringtech";
+    public static final String VERSION = "@version@";
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+	// TODO
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+	// TODO
+    }
+
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+	// TODO
+    }
+}
