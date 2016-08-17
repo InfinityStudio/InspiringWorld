376e826837cd6aa0d2b644123d81aac605f0de45
Yanbing Zhao <zzzz@mail.ustc.edu.cn>
2016-08-17 21:09:09 +0800

    Add capability storages of IWTechEnergy interfaces.

---
+++ src/tech/java/net/infstudio/inspiringworld/tech/common/capability/CapabilityIWTechEnergy.java
@@ -0,0 +1,43 @@
+package net.infstudio.inspiringworld.tech.common.capability;
+
+import com.google.common.base.Preconditions;
+
+import net.infstudio.inspiringworld.tech.api.energy.IIWTechEnergyConsumer;
+import net.infstudio.inspiringworld.tech.api.energy.IIWTechEnergyProvider;
+import net.minecraft.nbt.NBTBase;
+import net.minecraft.nbt.NBTTagString;
+import net.minecraft.util.EnumFacing;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.Capability.IStorage;
+
+public class CapabilityIWTechEnergy {
+    public static class StorageConsumer implements IStorage<IIWTechEnergyConsumer> {
+        // do nothing
+        @Override
+        public NBTBase writeNBT(Capability<IIWTechEnergyConsumer> capability, IIWTechEnergyConsumer instance,
+            EnumFacing side) {
+            return new NBTTagString("IWTechEnergyConsumer");
+        }
+
+        @Override
+        public void readNBT(Capability<IIWTechEnergyConsumer> capability, IIWTechEnergyConsumer instance,
+            EnumFacing side, NBTBase nbt) {
+            Preconditions.checkArgument("IWTechEnergyConsumer".equals(((NBTTagString) nbt).getString()));
+        }
+    }
+
+    public static class StorageProducer implements IStorage<IIWTechEnergyProvider> {
+        // do nothing
+        @Override
+        public NBTBase writeNBT(Capability<IIWTechEnergyProvider> capability, IIWTechEnergyProvider instance,
+            EnumFacing side) {
+            return new NBTTagString("IWTechEnergyProducer");
+        }
+
+        @Override
+        public void readNBT(Capability<IIWTechEnergyProvider> capability, IIWTechEnergyProvider instance,
+            EnumFacing side, NBTBase nbt) {
+            Preconditions.checkArgument("IWTechEnergyProducer".equals(((NBTTagString) nbt).getString()));
+        }
+    }
+}
